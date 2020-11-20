24467c3ff075256e278bba3014754e8bd0f8b5bf
garireo2549 <freedomgreo@gmail.com>
2020-11-20 14:58:08 +0900

    修正

--- db/init.d/todos.sql
+++ db/init.d/todos.sql
@@ -1,6 +1,5 @@
 create table todos(
     id VARCHAR (255) not null,
     name VARCHAR (255) not null,
-    todo VARCHAR (255) not null,
-    constraint log_files_PKC primary key (id)
+    todo VARCHAR (255) not null
 ) comment 'TODOテーブル';
